4de94b3f64b70711574b7c6b1fb2159369a10868
Felipe Fonseca <115054329+zarco98@users.noreply.github.com>
2023-04-20 20:51:37 -0500

    Add files via upload

---
+++ Challenge 1.java
@@ -0,0 +1,13 @@
+public class Solution{
+    //ESTA CLASE NO TIENE MAIN
+    
+    
+    public static int [] reporte(int [] participantes){
+        //EN ESTE ESPACIO PONER SU LÓGICA
+        
+        
+        
+        
+        
+    }
+}
