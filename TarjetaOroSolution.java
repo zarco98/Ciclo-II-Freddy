ec3413c595e4253b37fd048f1a66c6be759509a6
Felipe Fonseca <115054329+zarco98@users.noreply.github.com>
2023-04-24 10:25:35 -0500

    Update and rename TarjetaOro.java to TarjetaOroSolution.java

---
+++ TarjetaOroSolution.java
@@ -0,0 +1,42 @@
+public class TarjetaOro extends TarjetaCine {
+    //Atributos
+    private String[] beneficiosDescripcion = new String[]{"Boleto Gratis","Crispetas Gratis","Gaseosa Gratis"};
+    private boolean[] beneficiosEstado = new boolean[]{true, true, true};
+
+    //Constructor
+
+    public TarjetaOro(String idTarjeta, String nombreCompleto, String email, String telefono, int edad) {
+        super(idTarjeta, nombreCompleto, email, telefono, edad, 20);
+    }
+    //Método
+    public boolean redimirBeneficio(int posicionBeneficio)
+    {
+        if (beneficiosEstado[posicionBeneficio] == true)
+        {
+           beneficiosEstado[posicionBeneficio] = false; 
+           return true;
+        }
+        else
+        {
+           return false; 
+        }
+    }
+
+    //Getters y Setters
+
+    public String[] getBeneficiosDescripcion() {
+        return beneficiosDescripcion;
+    }
+
+    public void setBeneficiosDescripcion(String[] beneficiosDescripcion) {
+        this.beneficiosDescripcion = beneficiosDescripcion;
+    }
+
+    public boolean[] getBeneficiosEstado() {
+        return beneficiosEstado;
+    }
+
+    public void setBeneficiosEstado(boolean[] beneficiosEstado) {
+        this.beneficiosEstado = beneficiosEstado;
+    }
+}
