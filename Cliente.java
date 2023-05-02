e099e735c590f568a345d3e864b838d7f8b62fb7
Felipe Fonseca <115054329+zarco98@users.noreply.github.com>
2023-05-02 13:05:57 -0500

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,54 @@
+public class Cliente {
+    //ATRIBUTOS
+
+    //CONSTRUCTOR
+    public Cliente(String nombreCompleto, String documentoIdentidad, int totalAPagar, String fechaCompra, String numeroFactura) {
+        this.nombreCompleto = nombreCompleto;
+        this.documentoIdentidad = documentoIdentidad;
+        this.totalAPagar = totalAPagar;
+        this.fechaCompra = fechaCompra;
+        this.numeroFactura = numeroFactura;
+    }   
+    
+    //GETTERS Y SETTERS
+
+    public int getTotalAPagar() {
+        
+    }
+
+    public void setTotalAPagar(int totalAPagar) {
+        
+    }
+
+    public String getNombreCompleto() {
+        
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        
+    }
+
+    public String getDocumentoIdentidad() {
+        
+    }
+
+    public void setDocumentoIdentidad(String documentoIdentidad) {
+        
+    }
+
+    public String getFechaCompra() {
+        
+    }
+
+    public void setFechaCompra(String fechaCompra) {
+        
+    }
+
+    public String getNumeroFactura() {
+        
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        
+    }
+}
---
+++ Solution.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Solution{
+    //ESTA CLASE NO TIENE MAIN
+    
+    
+    public static Object[] reportes(ArrayList <Cliente> tienda){
+        //EN ESTE ESPACIO PONER SU LÓGICA
+        
+        
+        
+        
+        
+    }
+}
